459e05a2321db7826c44327dd8cdeb09db8cdf22
MisterChief53 <soderol3@hotmail.com>
2023-05-13 19:33:48 -0600

    added code to not delete .gitkeep in frames folder

--- Backend/cleanup.py
+++ Backend/cleanup.py
@@ -9,5 +9,5 @@ frames_path = 'Frames'
 for filename in os.listdir(frames_path):
     file_path = os.path.join(frames_path, filename)
 
-    if os.path.isfile(file_path):
+    if os.path.isfile(file_path) and filename != '.gitkeep':
         os.remove(file_path)
